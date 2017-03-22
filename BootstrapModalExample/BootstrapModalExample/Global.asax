a43c507ea869bcdde8e97ec0d6d6951b12e924b9
NathanKr <natankrasney@gmail.com>
2017-03-22 07:59:14 +0200

    Bootstrap Modal with Partial example

---
+++ BootstrapModalExample/BootstrapModalExample/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BootstrapModalExample.MvcApplication" Language="C#" %>
---
+++ BootstrapModalExample/BootstrapModalExample/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BootstrapModalExample.Startup))]
+namespace BootstrapModalExample
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

58bbe3ccf85c3e7228dddde2485145c5c98c0eb4
NathanKr <natankrasney@gmail.com>
2017-06-13 15:47:21 +0300

    File Write \ Read

---
+++ Telrad/ConsoleFileReadWrite/ConsoleFileReadWrite/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleFileReadWrite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // --- run Visual Studio as administrator if you get issues with writing to the disk
+            string path = @"c:\temp\MyTest.txt";
+            if (!File.Exists(path))
+            {
+                // Create a file to write to.
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    sw.WriteLine("Hello");
+                    sw.WriteLine("And");
+                    sw.WriteLine("Welcome");
+                }
+            }
+
+            // Open the file to read from.
+            using (StreamReader sr = File.OpenText(path))
+            {
+                string s = "";
+                while ((s = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+        }
+    }
+}

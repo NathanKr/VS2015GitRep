f427bc4998fbaa5cdde6db7bfe2e4eeca038c15e
NathanKr <natankrasney@gmail.com>
2017-06-27 12:16:45 +0300

    first mvc project

---
+++ Telrad/Web/ASP.net/MVC/MVC_empty/MVC_empty/Controllers/DefaultController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_empty.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            string virtualFile = "~/storage/books.txt";
+            string physicalFile = Server.MapPath(virtualFile);
+            string[] arFileLines = System.IO.File.ReadAllLines(physicalFile);
+
+            return View(arFileLines);
+        }
+    }
+}
---
+++ Telrad/Web/ASP.net/MVC/MVC_empty/MVC_empty/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC_empty.MvcApplication" Language="C#" %>

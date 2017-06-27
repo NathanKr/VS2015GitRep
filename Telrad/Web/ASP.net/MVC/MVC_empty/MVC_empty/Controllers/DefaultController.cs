701ebc8ddbc117d54d7aa19365b005fecfdda332
NathanKr <natankrasney@gmail.com>
2017-06-27 13:53:29 +0300

    simple form with validation

--- Telrad/Web/ASP.net/MVC/MVC_empty/MVC_empty/Controllers/DefaultController.cs
+++ Telrad/Web/ASP.net/MVC/MVC_empty/MVC_empty/Controllers/DefaultController.cs
@@ -3,19 +3,73 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MVC_empty.Models;
 
 namespace MVC_empty.Controllers
 {
     public class DefaultController : Controller
     {
-        // GET: Default
+        // GET: Index
         public ActionResult Index()
         {
             string virtualFile = "~/storage/books.txt";
             string physicalFile = Server.MapPath(virtualFile);
             string[] arFileLines = System.IO.File.ReadAllLines(physicalFile);
+            ;
+            List<Book> listBooks = new List<Book>();
 
-            return View(arFileLines);
+            foreach (string strBookLine in arFileLines)
+            {
+                string[] arLineSplited = strBookLine.Split(',');
+
+                Book book = new Book
+                {
+                    Name = arLineSplited[0],
+                    Category = arLineSplited[1],
+                    PageNum = int.Parse(arLineSplited[2])
+                };
+                listBooks.Add(book);
+            }
+
+            return View(listBooks);
+        }
+
+        // GET : DoesBookExist
+
+        [HttpGet]
+        public ActionResult DoesBookExist()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult HandleDoesBookExist(string name)
+        {
+            bool bIsValid = name.Length > 0;
+
+            if (!bIsValid)
+            {
+                ViewBag.Error = "name is not valid";
+                return View("DoesBookExist");
+            }
+
+            bool bDoesExist = false;
+            string virtualFile = "~/storage/books.txt";
+            string physicalFile = Server.MapPath(virtualFile);
+            string[] arFileLines = System.IO.File.ReadAllLines(physicalFile);
+            foreach (string strBookLine in arFileLines)
+            {
+                string[] arLineSplited = strBookLine.Split(',');
+                if (arLineSplited[0] == name)
+                {
+                    bDoesExist = true;
+                    break;
+                }
+            }
+
+
+            return View(bDoesExist);
         }
     }
 }
+    

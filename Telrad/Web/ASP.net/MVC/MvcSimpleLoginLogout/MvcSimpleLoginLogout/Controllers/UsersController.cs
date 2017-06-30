2239c292daf48cb319ea6a74f5f6a186f83925b9
NathanKr <natankrasney@gmail.com>
2017-06-30 15:18:41 +0300

    simple MVC login logout sample

---
+++ Telrad/Web/ASP.net/MVC/MvcSimpleLoginLogout/MvcSimpleLoginLogout/Controllers/UsersController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcSimpleLoginLogout.Controllers
+{
+    public class UsersController : Controller
+    {
+        [HttpGet]
+        public ActionResult Home()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string userName , string password) // use here same name Login - more intuitive
+        {
+            // -- no need for validation because i put nothing to storage
+            bool bDoesExist = (userName == "Avi") && (password == "31aB_$");// -- hard code
+            if (!bDoesExist)
+            {
+                ViewBag.LoginError = "Login is not valid";
+                return View("Login");
+            }
+
+
+            Session["userSessionKey"] = userName;
+            return RedirectToAction("Home"); ;
+        }
+
+        [HttpGet]
+        public ActionResult Logout()
+        {
+            Session["userSessionKey"] = null;
+            return RedirectToAction("Home");
+        }
+    }
+}
---
+++ Telrad/Web/ASP.net/MVC/MvcSimpleLoginLogout/MvcSimpleLoginLogout/Readme.txt
@@ -0,0 +1,7 @@
+﻿1. simple implementation just to show how to login logout using session
+
+2. login is allowed only for user Avi and password 31aB_$
+
+3. we have two screens : home and login
+
+4. home has 2 links for login and logout and welcome logged in user

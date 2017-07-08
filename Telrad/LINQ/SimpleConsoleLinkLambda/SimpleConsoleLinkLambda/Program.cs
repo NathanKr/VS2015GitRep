cc188315b166806df51f0ae790d03057798dc89b
NathanKr <natankrasney@gmail.com>
2017-07-08 08:27:58 +0300

    LINQ with Lambda

---
+++ Telrad/LINQ/SimpleConsoleLinkLambda/SimpleConsoleLinkLambda/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleConsoleLinkLambda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = { "hello", "world", "this", "is", "LINQ" };
+            //Get only short words using Lambda expression
+            IEnumerable<string> shortWords =  words.Where(w => w.Length <= 4);
+
+            //Print each word out
+            foreach (var word in shortWords)
+            {
+                Console.WriteLine(word);
+            }
+            Console.ReadLine();
+        }
+    }
+}

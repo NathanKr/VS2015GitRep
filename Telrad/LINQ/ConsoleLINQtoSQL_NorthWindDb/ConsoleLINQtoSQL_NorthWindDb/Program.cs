0985aca2ec655def5c1c583110fc03d9d4645eb4
NathanKr <natankrasney@gmail.com>
2017-07-12 14:27:35 +0300

    simple LINQtoSQL console data base used in Northwind

---
+++ Telrad/LINQ/ConsoleLINQtoSQL_NorthWindDb/ConsoleLINQtoSQL_NorthWindDb/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleLINQtoSQL_NorthWindDb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string strConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;Initial Catalog=Northwind;Integrated Security=True;Connect Timeout=15;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            DataClasses1DataContext nw = new DataClasses1DataContext(strConnectionString);
+
+            var resultQuery = from cust in nw.Customers
+                where cust.City == "Paris"
+                select cust.CompanyName;
+
+            foreach (var item in resultQuery)
+            {
+                Console.WriteLine(item);
+            }
+
+            var resultLambda = nw.Customers.Where(x => x.City == "Paris").Select(x => x.CompanyName);
+                              
+
+            foreach (var item in resultLambda)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}

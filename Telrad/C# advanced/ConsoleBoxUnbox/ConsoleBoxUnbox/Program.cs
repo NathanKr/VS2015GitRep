03931c208dee0f5ff7d1acc1f183c6bc99b84c8a
NathanKr <natankrasney@gmail.com>
2017-10-09 16:15:55 +0300

    boxing & unboxing

---
+++ Telrad/C# advanced/ConsoleBoxUnbox/ConsoleBoxUnbox/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleBoxUnbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 36;
+            object obj1 = a;    
+            int b = (int)obj1;  
+
+            ArrayList arrayList = new ArrayList();
+            arrayList.Add(1);
+            arrayList.Add(true);
+            arrayList.Add("hello");
+            foreach (var item in arrayList)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}

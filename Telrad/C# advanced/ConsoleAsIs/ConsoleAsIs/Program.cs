2005d63a3ea650af21869aeadd020138cd4c700f
NathanKr <natankrasney@gmail.com>
2017-10-09 17:53:31 +0300

    as is dynamic

---
+++ Telrad/C# advanced/ConsoleAsIs/ConsoleAsIs/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAsIs
+{
+    class Program
+    {
+        class Base {};
+
+        class Derived : Base {};
+
+        static bool IsDerivedVer1(Base b){
+            return b is Derived;
+        }
+
+        static bool IsDerivedVer2(Base b){
+            Derived d = b as Derived;
+            return d != null;
+        }
+
+        static void Main(string[] args)
+        {
+            Base b = new Base();
+            Derived d = new Derived();
+
+            Console.WriteLine("is derived : {0}", IsDerivedVer1(b));
+            Console.WriteLine("is derived : {0}", IsDerivedVer1(d));
+
+            Console.WriteLine("is derived : {0}", IsDerivedVer2(b));
+            Console.WriteLine("is derived : {0}", IsDerivedVer2(d));
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Telrad/C# advanced/ConsoleDynamic/ConsoleDynamic/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleDynamic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            dynamic a;
+            a = 1;
+            a++;
+            Console.WriteLine(a);
+            Console.ReadKey();
+        }
+    }
+}

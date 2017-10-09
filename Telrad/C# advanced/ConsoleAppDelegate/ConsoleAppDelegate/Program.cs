68bc85c41be7c762c4845565cb019a05b65b6ad6
NathanKr <natankrasney@gmail.com>
2017-10-09 08:23:11 +0300

    delegate lambda expression

---
+++ Telrad/C# advanced/ConsoleAppDelegate/ConsoleAppDelegate/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppDelegate
+{
+    class Program
+    {
+        public delegate int BasicOperation(int num1, int num2);
+
+        static int Add(int a, int b){
+            return a + b;
+        }
+
+        static int Mul(int a, int b)
+        {
+            return a * b;
+        }
+
+        static void DoOperationAndPrint(int a, int b, BasicOperation operation)
+        {
+            Console.WriteLine("result : {0}",operation(a, b));
+        }
+
+        static void Main(string[] args)
+        {
+            int num1 = 2, num2 = 3;
+            BasicOperation add = Add, mul = Mul;
+            DoOperationAndPrint(num1, num2, mul);
+            DoOperationAndPrint(num1, num2, add);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Telrad/C# advanced/ConsoleLambdaExpression/ConsoleLambdaExpression/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleLambdaExpression
+{
+    class Program
+    {
+        delegate int BasicOperation(int num1, int num2);
+
+        static void Main(string[] args)
+        {
+            BasicOperation mul = (int a, int b) => { return a * b; };
+            BasicOperation add = (int a, int b) => { return a + b; };
+            Console.WriteLine(add(1,2));
+            Console.WriteLine(mul(3, 4));
+            Console.ReadKey();
+        }
+    }
+}

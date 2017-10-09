f0b07298b8a216413502f31895fa89ec49eb5bc5
NathanKr <natankrasney@gmail.com>
2017-10-09 07:24:53 +0300

    c# advanced

---
+++ Telrad/C# advanced/ConsoleAsyncAwait/ConsoleAsyncAwait/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAsyncAwait
+{
+    class Program
+    {
+        public static async Task<string> GetWebPage()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string res = await client.GetStringAsync("http://www.google.com");
+                return res;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            string page = GetWebPage().Result;
+            Console.WriteLine(page);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Telrad/C# advanced/ConsolePrallel/ConsolePrallel/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsolePrallel
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Parallel.For(0, 200, i =>
+            {
+                write(i);
+            });
+
+            waitForKey();
+
+            var ar = Enumerable.Range(0, 10);
+            Parallel.ForEach(ar,i => {
+                write(i);
+            });
+
+            waitForKey();
+        }
+
+        private static void waitForKey()
+        {
+            Console.WriteLine("hit a key"); Console.ReadKey();
+        }
+
+        private static void write(int i)
+        {
+            Console.WriteLine("task id : {0} , i : {1}", Task.CurrentId, i);
+        }
+
+    }
+}

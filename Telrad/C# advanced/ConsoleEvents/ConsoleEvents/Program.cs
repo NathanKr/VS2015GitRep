85cc5c023f5cd1e59a5e9379c42a35311c1b7c49
NathanKr <natankrasney@gmail.com>
2017-10-09 13:14:50 +0300

    Events

---
+++ Telrad/C# advanced/ConsoleEvents/ConsoleEvents/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleEvents
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Publish pub = new Publish();
+            pub.OnChanged += () => { Console.WriteLine("handle 1 event"); };
+            pub.OnChanged += () => { Console.WriteLine("handle 2 event"); };
+
+            pub.RaiseEvent();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Telrad/C# advanced/ConsoleEvents/ConsoleEvents/Publish.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleEvents
+{
+    class Publish
+    {
+        public Action OnChanged { get; set; }
+
+        public void RaiseEvent()
+        {
+            if(OnChanged != null)
+            {
+                OnChanged();
+            }
+        }
+    }
+}

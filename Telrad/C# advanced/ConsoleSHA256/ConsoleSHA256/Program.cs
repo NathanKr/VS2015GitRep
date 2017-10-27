9c30e2bd80cd23ad51c1ae8dcdeaa6a7792f4c54
NathanKr <natankrasney@gmail.com>
2017-10-27 18:16:48 +0300

    SHA256

---
+++ Telrad/C# advanced/ConsoleSHA256/ConsoleSHA256/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleSHA256
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UnicodeEncoding unicode = new UnicodeEncoding();
+            SHA256 sha = SHA256.Create();
+            string strText1 = "some text 1";
+            byte[] arHashCodeSha256 = sha.ComputeHash(unicode.GetBytes(strText1));
+            foreach (var item in arHashCodeSha256)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.Read();
+        }
+    }
+}
